7b2c242b6524950e501447ae93de57907261703b
Edward Fish <EdwardLFish@gmail.com>
2015-11-01 22:50:20 +0200

    I try run script with function trim(text) No result!

--- Test_For_JS/src/TestJSAppl.java
+++ Test_For_JS/src/TestJSAppl.java
@@ -1,15 +1,21 @@
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import javax.script.*;
 
 public class TestJSAppl {
 
-	public static void main(String[] args) throws ScriptException {
+	public static void main(String[] args) throws ScriptException, FileNotFoundException {
 		ScriptEngineManager manager=new ScriptEngineManager();
-		ScriptEngine engine=manager.getEngineByName("js");
-		Object obj=engine.eval("");
+		ScriptEngine engine=manager.getEngineByName("javascript");
+		FileReader fr = new FileReader("tests.js");
+		Bindings bindings = new SimpleBindings();
+		bindings.put("text", "Hello!     ");
+		Object obj=engine.eval(fr, bindings);
+		//String str=(String) obj;
 		System.out.println(obj);
-
+		
 	}
+
 
 }
